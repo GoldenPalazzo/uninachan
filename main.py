a034d76602dfa48f5f7d3a4f71b3201220f3dd04
Francesco Palazzo <francesco@palazzo.me>
2026-03-13 13:16:53 +0100

    Esempio con postgres

--- main.py
+++ main.py
@@ -1,18 +1,40 @@
 import random
 from fastapi import FastAPI
+from pydantic import BaseModel
+import psycopg
+from psycopg.rows import class_row
 
+DATABASE_URL = "postgresql://postgres:example@localhost:5432/postgres"
 app = FastAPI()
-utenti = []
+
+class User(BaseModel):
+    name: str
+
+def get_users() -> list[User]:
+    with psycopg.connect(DATABASE_URL) as conn:
+        with conn.cursor(row_factory=class_row(User)) as cur:
+            cur.execute("SELECT * FROM users")
+            return cur.fetchall()
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {
+        "message": "Hello World",
+        'existing_users': get_users()
+    }
 
 @app.get('/createuser')
-async def newuser():
-    new_user = f'NuovoUtente{random.randint(1,100)}'
-    utenti.append(new_user)
+async def newuser(username: str):
+    with psycopg.connect(DATABASE_URL) as conn:
+        with conn.cursor() as cur:
+            try:
+                cur.execute("INSERT INTO users (name) VALUES (%s)", (username,))
+                conn.commit() # Commit the changes to the database
+            except psycopg.errors.UniqueViolation as e:
+                return {'error': f'User {username} already exists'}
+    users = get_users()
+    print(users)
     return {
-        'message': f'Creato nuovo utente {new_user}',
-        'existing_users': utenti
+        'message': f'Creato nuovo utente {username}',
+        'existing_users': get_users()
     }
